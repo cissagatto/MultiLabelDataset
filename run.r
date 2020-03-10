351d61c2bbb718ee24b8fccf9899a6af4b291818
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2020-03-10 15:26:24 -0300

    Update run.r

--- run.r
+++ run.r
@@ -1,11 +1,11 @@
-  ##################################################################################################
+##################################################################################################
 # Elaine Cecilia Gatto | Prof. Dr. Ricardo Cerri | Prof. Dr. Mauri Ferrandin                     #
 # www.professoracissagatto.com.br                                                                #
 # Federal University of Sao Carlos (UFSCar: https://www2.ufscar.br/) Campus Sao Carlos           #
 # Computer Department (DC: https://site.dc.ufscar.br/)                                           #
 # Program of Post Graduation in Computer Science (PPG-CC: http://ppgcc.dc.ufscar.br/)            #
 # Bioinformatics and Machine Learning Group (BIOMAL: http://www.biomal.ufscar.br/)               #
-# Algorithm 2 - Run                                                                              #
+# Algorithm 3 - Run                                                                              #
 ##################################################################################################
 
 
@@ -22,103 +22,106 @@ library("stringr")
 # INTERNAL LIBRARIES                               #
 ####################################################
 source("utils.r")
-
-  
-  
-####################################################
-# EXECUTE                                          #
-####################################################
-cat("\n|========== START Separated Instances Per Labels ==========|\n")
+source("MultiLabelDataset.R")
 sf = setFolder()
 FolderRoot = sf$Folder
 setwd(FolderRoot)
+
+
+
+
+####################################################
+# EXECUTE                                          #
+####################################################
 diretorios = directories()
+# escolher TIPO igual a TRAIN ou TEST ou ALL
+conjunto = "train"
+fin = c(diretorios$dirCSV)
+fon = folderNames(c(fn), tipo = conjunto)
+fnf = fileNamesFinal(c(fon))
+
+setwd(FolderRoot)
 datasets = read.csv("datasets.csv")
-fn = fileNames()
-ffnn = folderNames(c(fn$fileNames))
-fnf = fileNamesFinal(c(ffnn$folderNames))
 
+dataset = c(0)
+card = c(0)
+dens = c(0)
+dime = c(0)
+instancias = c(0)
+dados = data.frame(dataset, card, dens, dime, instancias)
 
-####################################################
-# IPL                                              #
-####################################################
-j = 1
-for(j in 1:d$n_CSV){
-  cat("\n Dataset: ", fn$fileNames[j], "\n")
-  ds = datasets[j,]
+i = 1
+for(i in 1:diretorios$n_CSV){
+  cat("\n Dataset: ", fn[i], "\n")
+  ds = datasets[i,]
   info = infoDataSet(ds)
-  instancesPerLabels(ffnn$folderNames[j], fn$fileNames[j], fnf[j], ds$AttStart, ds$AttEnd, 
-                     ds$LabStart, ds$LabEnd, ds$Instances, ds$Labels)
-  # instancesPerLabelsSpace(ffnn$folderNames[j], fn$fileNames[j], fnf[j], ds$AttStart, ds$AttEnd, ds$LabStart, ds$LabEnd, ds$Instances, ds$Labels)
-  j = j + 1
+  setwd(diretorios$folderCSV)
+  tudo = read.csv(fn[i])
+  instancias = nrow(tudo)
+  
+  # atributos previsores
+  cat("\nSeparando os atributos previsores!")
+  setwd(diretorios$folderAS)
+  atributos = data.frame(tudo[,info$attStart:info$attEnd])
+  write.csv(atributos, paste(fon[i], "_attributes_", conjunto, "_.csv", sep=""), row.names = FALSE)
+  
+  # atributos alvo
+  cat("\nSeparando os atributos alvo!")
+  setwd(diretorios$folderLS)
+  classes = data.frame(tudo[,info$labStart:info$labEnd])
+  write.csv(classes, paste(fon[i], "_labels_", conjunto, "_.csv", sep=""), row.names = FALSE)
+  
+  # salvando os rotulos
+  cat("\nSeparando os nomes dos atributos alvo!")
+  rotulos = c(colnames(classes))
+  setwd(diretorios$folderL)
+  write.csv(rotulos, paste(fon[i], "_labels_only.csv", sep=""))
+  
+  # cardinalidade
+  cat("\nCalculando a cardinalidade!")
+  card = cardinality(classes)
+  
+  # densidade
+  cat("\nCalculando a densidade!")
+  dens = density(classes)
+  
+  # dimensionalidade
+  cat("\nCalculando a dimensionalidade!")
+  dimen = as.numeric(info$predictiveAttributes/info$instances)
+  
+  # salvando os numeros
+  cat("\nSalvando as informacoes!")
+  dataset = fn[i]
+  dados = rbind(dados, data.frame(dataset, card, dens, dime, instancias))
+  setwd(sf$Folder)
+  write.csv(dados, "_sumario_datasets_", conjunto, "_.csv", append = TRUE)
+  
+  # limpar
+  cat("\nLimpando os objetos criados!")
+  rm(tudo)
+  rm(atributos)
+  rm(classes)
+  rm(rotulos)
+  
+  cat("\nIncrementando!")
+  i = i + 1
+  
+  cat("\nColetando lixo!\n")
   gc()
 }
 
-####################################################
-# SEPARATE SPACES                                  #
-####################################################
-
-# dataset = c(0)
-# card = c(0)
-# dens = c(0)
-# dime = c(0)
-# dados = data.frame(dataset, card, dens, dime)
-
-# i = 1
-#for(i in 1:d$n_CSV){
-  #cat("\n Dataset: ", fn$fileNames[i], "\n")
-  #ds = datasets[i,]
-  #info = infoDataSet(ds)
-  #setwd(d$folderCSV)
-  #tudo = read.csv(fn$fileNames[i])
+j = 1
+for(j in 1:diretorios$n_CSV){
+  cat("\n Dataset: ", fn[j], "\n")
+  ds = datasets[j,]
+  info = infoDataSet(ds)
   
-  # atributos previsores
-  #cat("\nSeparando os atributos previsores!")
-  #setwd(d$folderAS)
-  #atributos = data.frame(tudo[,info$attStart:info$attEnd])
-  #write.csv(atributos, paste(ffnn$folderNames[i], ".csv", sep=""), row.names = FALSE)
+  instancesPerLabels(ds$ID[j], fon[j], fn[j], fnf[j], ds$AttStart, ds$AttEnd, ds$LabStart, ds$LabEnd, ds$Instances, 
+                     ds$Labels, conjunto)
   
-  # atributos alvo
-  #cat("\nSeparando os atributos alvo!")
-  #setwd(d$folderLS)
-  #classes = data.frame(tudo[,info$labStart:info$labEnd])
-  #write.csv(classes, paste(ffnn$folderNames[i], ".csv", sep=""), row.names = FALSE)
+  instancesPerLabelsSpace(ds$ID[j], fon[j], fn[j], fnf[j], ds$AttStart, ds$AttEnd, ds$LabStart, ds$LabEnd, ds$Instances, 
+                          ds$Labels, conjunto)
   
-  # salvando os rotulos
-  #cat("\nSeparando os nomes dos atributos alvo!")
-  #rotulos = c(colnames(classes))
-  #setwd(d$folderL)
-  #write.csv(rotulos, paste(ffnn$folderNames[i], ".csv", sep=""))
-  
-  # cardinalidade
-  #cat("\nCalculando a cardinalidade!")
-  #card = cardinality(classes)
-  
-  # densidade
-  #cat("\nCalculando a densidade!")
-  #dens = density(classes)
-  
-  # dimensionalidade
-  #cat("\nCalculando a dimensionalidade!")
-  #dimen = as.numeric(info$predictiveAttributes/info$instances)
-  
-  # salvando os numeros
-  #cat("\nSalvando as informacoes!")
-  #dataset = fn$fileNames[i]
-  #dados = rbind(dados, data.frame(dataset, card, dens, dime))
-  #setwd(sf$Folder)
-  #write.csv(dados, "sumario_datasets.csv", append = TRUE)
-  
-  # limpar
-  #cat("\nLimpando os objetos criados!")
-  #rm(tudo)
-  #rm(atributos)
-  #rm(classes)
-  #rm(rotulos)
-  
-  # cat("\nIncrementando!")
-  # i = i + 1
-   
-#  cat("\nColetando lixo!\n")
-#  gc()
-# }
+  j = j + 1
+  gc()
+}
